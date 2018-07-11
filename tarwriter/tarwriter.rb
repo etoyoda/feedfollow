25e0effec789ffb737c9a71b320bb80ae6d2d4db
TOYODA Eizi <toyoda.eizi@gmail.com>
2018-07-11 17:26:23 +0000

    tar writer (incomplete)

--- tarwriter/tarwriter.rb
+++ tarwriter/tarwriter.rb
@@ -2,6 +2,8 @@
 
 class TarWriter
 
+  include File::Constants
+
   def TarWriter::open fnam, mode
     tar = TarWriter.new(fnam, mode)
     yield(tar) if block_given?
@@ -9,7 +11,39 @@ class TarWriter
     fnam
   end
 
-  def initialize fnam, mode
+  def initialize file, mode = 'w'
+    @io = nil
+    if IO === file
+    then
+      file
+    else
+      case mode
+      when 'x'
+        File.open(file, WRONLY|CREAT|EXCL|TRUNC).set_encoding('BINARY')
+      else
+	File.open(file, fmode)
+      end
+    end
+  end
+
+  def header bfnam, size, cksum = "       "
+    mode = '0644'
+    uid = gid = 'nobody'
+    fmt = ''
+    [bfnam, mode, uid, gid, csize, mtime, cksum, typeflag, linkname, magic,
+    version, uname, gname, devmajor, devminor, prefix].pack(fmt)
+  end
+
+  def add fnam, content
+    bfnam = String(fnam, encoding: "BINARY")
+    raise "too long filename #{fnam}" if bfnam.size >= 100
+    bcontent = String(content, encoding: "BINARY")
+    testhdr = header(bfnam, bcontent.size)
+
+  end
+
+  def flush
+    @io.flush
   end
 
 end
--- tarwriter/test.sh
+++ tarwriter/test.sh
@@ -1,2 +1,10 @@
-ruby tarwriter.rb ztest.tar tarwriter.rb
-tar tvf ztesttar
+if ! ruby tarwriter.rb ztest.tar tarwriter.rb
+then
+  echo fail 1
+  exit
+fi
+if ! tar tvf ztest.tar
+then
+  echo fail 2
+  exit
+fi
