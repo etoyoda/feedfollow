386f6ae4eec16034b4007386dbdc28dbecaf5a48
TOYODA Eizi <toyoda.eizi@gmail.com>
2018-12-06 03:12:07 +0900

    now it looks like wor

---
+++ run-feedstore.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+PATH=/bin:/usr/bin
+
+: ${phase:?} ${base:?} ${reftime:?} ${datedir:?} ${prefix:?}
+
+: ${ruby:=/usr/bin/ruby}
+: ${syndl:=${prefix}/bin/syndl.rb}
+: ${feeddir:='http://www.data.jma.go.jp/developer/xml/feed/'}
+: ${ca:='--ca=/etc/ssl/certs/'}
+
+if tty -s; then
+  ruby="${ruby} -w"
+fi
+
+set -e
+
+$ruby ${prefix}/bin/feedstore.rb jmx-lmt.db jmx-${reftime} $feeddir/extra.xml
+
