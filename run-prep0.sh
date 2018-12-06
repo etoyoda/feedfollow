ac5e8bec31ad831135287f1c4d2a296ff8535b44
TOYODA Eizi <toyoda.eizi@gmail.com>
2018-12-06 18:32:12 +0900

    debug - datedir is now fullpath

--- run-prep0.sh
+++ run-prep0.sh
@@ -21,14 +21,16 @@ cd ${base}
 # aborts by -e if prefix or base is ill-configured
 
 incomplete="`readlink incomplete || :`"
-if [ X"${incomplete}" != X"${datedir}" ]; then
+if [ X"${incomplete}" != X"${reftime}" ]; then
   if [ -d "${incomplete}" ]; then
     yesterday="`basename $incomplete .new`"
     mv -f "${incomplete}" "${yesterday}"
-    rm -f latest && ln -s "${yesterday}" latest
+    rm -f latest
+    ln -s "${yesterday}" latest
     logger --id=$$ -p news.info "latest -> ${yesterday}, incomplete -> ${datedir}"
   fi
-  rm -f incomplete && ln -s ${datedir} incomplete
+  rm -f incomplete
+  ln -s ${datedir} incomplete
 fi
 cd ${datedir}
 
