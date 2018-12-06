64a7c0e9efbf0ac3007ac55810d7815d70000b78
TOYODA Eizi <toyoda.eizi@gmail.com>
2018-12-06 18:21:01 +0900

    bugfix - directory name

--- run-prep0.sh
+++ run-prep0.sh
@@ -21,13 +21,13 @@ cd ${base}
 # aborts by -e if prefix or base is ill-configured
 
 incomplete="`readlink incomplete || :`"
-if [ X"${incomplete}" != X"${reftime}" ]; then
+if [ X"${incomplete}" != X"${datedir}" ]; then
   if [ -d "${incomplete}" ]; then
     yesterday="`basename $incomplete .new`"
     mv -f "${incomplete}" "${yesterday}"
     rm -f latest
     ln -s "${yesterday}" latest
-    logger --id=$$ -p news.info "latest -> ${yesterday}, incomplete -> ${datedir}"
+    logger --tag run-prep --id=$$ -p news.info "latest -> ${yesterday}, incomplete -> ${datedir}"
   fi
   rm -f incomplete
   ln -s ${datedir} incomplete
