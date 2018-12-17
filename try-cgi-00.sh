aa4dab99c95cfbf5fbb9a38b1a105f15de8695ad
TOYODA Eizi <toyoda.eizi@gmail.com>
2018-12-18 01:10:18 +0900

    test to be useful when we change syndl.cgi someday

---
+++ try-cgi-00.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -Ceuo pipefail
+
+PATH=/bin:/usr/bin:/usr/local/bin
+
+nwp=/nwp
+idx1=${nwp}/p0/incomplete/jmx-2*.idx1
+datedir=$(readlink ${nwp}/p0/incomplete)
+
+set $(gdbm ${idx1} select ^u | head -1)
+uuid=$1
+offset=$2
+
+: ${uuid:?} ${offset:?}
+
+test ! -f z.out || rm z.out
+
+cgi=/usr/lib/cgi-bin/syndl.cgi
+#cgi=./syndl.cgi
+
+PATH_INFO=/entry/${datedir}/jmx/${uuid} REQUEST_METHOD=GET ruby -d $cgi > z.out
