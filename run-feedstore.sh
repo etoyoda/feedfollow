bcd0865b9bc365984bd1c878236d479edb431dea
TOYODA Eizi <toyoda.eizi@gmail.com>
2019-09-27 00:04:47 +0000

    reverted - magic hack didnt work

--- run-feedstore.sh
+++ run-feedstore.sh
@@ -9,7 +9,7 @@ export phase base reftime datedir nwp
 : ${ruby:=/usr/bin/ruby}
 : ${feeddir:='https://www.data.jma.go.jp/developer/xml/feed'}
 : ${ca:='/etc/ssl/certs/'}
-: ${magic:=25}
+: ${magic:=20}
 
 cd ${datedir}
 
