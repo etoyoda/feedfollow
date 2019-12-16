8c52ac81e19f65a0a0936fd6c6c7c3a90c3c9307
TOYODA Eizi <toyoda.eizi@gmail.com>
2019-12-16 00:28:25 +0000

    make sure program runs

---
+++ defunct-delete.rb
@@ -0,0 +1,88 @@
+#!/usr/bin/ruby
+
+
+class App
+
+  def diag msg
+    return unless $VERBOSE
+    $stderr.puts msg
+    msg
+  end
+
+  def initialize argv
+    $VERBOSE = true if $stderr.tty?
+    @argv = argv
+    if @argv.empty?
+      @argv = ['/var/log/apache2/access.log.1',
+        '/var/log/apache2/access.log.7.gz']
+      diag "argv = #{@argv.inspect}"
+    end
+    @archhosts = {}
+    dotfile
+    @n = 0
+  end
+
+  def dotfile
+    fnam = File.join(ENV['HOME'], '.archhost')
+    File.open(fnam, 'r') {|fp|
+      for line in fp
+        next unless /ARCHHOST\s*=\s*(\S+)/ === line
+        @archhosts[$1] = true
+      end
+    }
+  rescue Errno::ENOENT
+  end
+
+  def checkfile clie, path, size
+    return if /\/$/ === path
+    return unless /^\/nwp\/a/ === path
+    diag "> #{clie} #{path} #{size}"
+    unless @archhosts[clie] then
+      return diag("unregisterd client: #{clie}")
+    end
+    begin
+      sz = File.stat(path).size
+      size = size.to_i
+    rescue Errno::ENOENT
+      return diag("already removed: #{path}")
+    end
+    if sz > size then
+      return diag("partial transfer: #{path} #{sz}/#{size}")
+    end
+    File.unlink(path)
+    @n += 1
+    diag("ok removed: #{path}")
+  end
+
+  def logfile ifp
+    ifp.each_line{|line|
+      next unless /^\S+ (\S+) .*\] "GET (\S+) HTTP\/1\.." 200 (\d+)/ === line
+      checkfile($1, $2, $3)
+    }
+  end
+
+  def endmsg
+    return if @n > 0
+    msg = "no file removed this time - check config"
+    $stderr.puts $msg
+    puts $msg
+    exit 16
+  end
+
+  def run
+    @argv.sort.each{|file|
+      diag "reading #{file}"
+      case file
+      when /\.gz$/ then
+        require 'zlib'
+        Zlib::GzipReader.open(file){|ifp| logfile(ifp) }
+      else
+        File.open(file){|ifp| logfile(ifp) }
+      end
+    }
+    endmsg
+  end
+
+end
+
+App.new(ARGV).run
--- run-a0-defunct.sh
+++ run-a0-defunct.sh
@@ -1,46 +1,6 @@
 #!/bin/bash
 set -Ceuo pipefail
 
-: ${ACLOG:='/var/log/apache2/access.log.1'}
 : ${RUBY:='/usr/bin/ruby'}
 
-if [ -f ${HOME}/.archhost ]; then
-  . ${HOME}/.archhost
-fi
-: ${ARCHHOST:='localhost'}
-export ARCHHOST
-
-${RUBY} -- - ${ACLOG} <<-'END_OF_RUBY'
-  archhost = ENV['ARCHHOST']
-  n = 0
-  ARGF.each_line {|line|
-    next unless /^\S+ (\S+) .*\] "GET (\S+[^\/]) HTTP\/1\.\d+" 200 (\d+) / === line
-    clie, path, size = $1, $2, $3
-    size = size.to_i
-    next unless /^\/nwp\/a0\// === path
-    if clie != archhost
-      $stderr.puts "BADHOST #{clie} #{archhost}"
-      next
-    end
-    begin
-      fst = File.stat(path)
-    rescue Errno::ENOENT
-      $stderr.puts "ENOENT #{path}"
-      next
-    end
-    unless size > fst.size
-      $stderr.puts "MIDWAY #{path} #{fst.size} #{size} #{size * 1000 / fst.size}"
-      next
-    end
-    begin
-      # $stderr.puts "unlink #{path}"
-      File.unlink(path)
-      n += 1
-    rescue Errno::EACCES
-      $stderr.puts "Permission denied: unlink #{path}"
-      next
-    end
-  }
-  $stderr.puts "no deletion today - check #{archhost}" if n.zero?
-  puts "no deletion today - check #{archhost}" if n.zero?
-END_OF_RUBY
+$RUBY /nwp/bin/defunct-delete.rb
